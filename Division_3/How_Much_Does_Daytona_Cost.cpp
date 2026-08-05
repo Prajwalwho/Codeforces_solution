7e7a4c178f5f6016db951dfc88095e5325b65dc7
Prajwalwho <prajwalsingh1376@gmail.com>
2026-08-06 00:24:08 +0530

    how much does daytona cost

---
+++ Division_3/How_Much_Does_Daytona_Cost.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        int k;
+        cin >> n >> k;
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == k)
+                cnt++;
+        }
+        if (cnt != 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
